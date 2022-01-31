651ec900a69cf9afd9b419cdf47296560c5d092b
caden-m <90535137+caden-m@users.noreply.github.com>
2022-01-30 20:54:22 -0700

    Mission3 Committ

---
+++ Mission3/Mission3/Models/CalculatorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Mission3.Models
+{
+    public class CalculatorModel
+    {
+        
+        [Range(0,100)]
+        public int Assignments { get; set; }
+        public int GroupProject { get; set; }
+        public int Quizzes { get; set; }
+        public int Exams { get; set; }
+        public int Intex { get; set; }
+
+
+    }
+}
+
---
+++ Mission3/Mission3/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//document.getElementById("calcButton").addEventListener("click", function () {
+//    Assignments = document.getElementById("numAssignments").value 
+//    groupProject = document.getElementById("numGroupProject").value 
+//    Quizzes = document.getElementById("numQuizzes").value 
+//    Exams = document.getElementById("numExams").value 
+//    Intex = document.getElementById("numIntex").value)
+//    totalGrade = Assignments + groupProject + Quizzes + Exams + Intex  
+//})
+
+
+
+//document.getElementById("calcButton").addEventListener("click", function () {
+//    alert("Assignments: " + document.getElementById("numAssignments").value +
+//        ", Group Projects: " + document.getElementById("numGroupProject").value +
+//        ", Quizzes: " + document.getElementById("numQuizzes").value +
+//        ", Exams: " + document.getElementById("numExams").value +
+//        ", INTEX: " + document.getElementById("numIntex").value);
+
+//})
+
+$("#calcButton").click(function () {
+    alert("Assignments: " + $("#numAssignments").val() +
+        ", Group Projects: " + $("#numGroupProject").val() +
+        ", Quizzes: " + $("#numQuizzes").val() +
+        ", Exams: " + $("#numExams").val() +
+        ", INTEX: " + $("#numIntex").val() +
+        ", Total Grade: ");
+        
+})
+
+
